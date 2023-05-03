02c3324558a7ae6951f55043584c733ca2f484ab
MHIDotito <otito@vasak.info>
2023-05-03 19:48:04 +0200

    some edits

--- games.js
+++ games.js
@@ -4,16 +4,16 @@ function reqListener(response) {
     const data = JSON.parse(this.responseText)
     for (const game of data) {
         gamesDiv.innerHTML += `
-        <a class="d-block text-light link-light text-decoration-none col py-3">
-            <article class="card shadow-sm  bg-secondary text-light border">
-                <img class="card-img-top" src="${game.img}" alt="Obrázek ${game.name}">
+        <div class="d-block text-light link-light text-decoration-none col py-3">
+            <div class="card shadow-sm  bg-secondary text-light border">
+                <img class="card-img-top" src="${game.img}" alt="Obrázek ${game.name}" style="border: 2px solid #6c757d">
                 <div class="card-body">
                     <h3 class="card-text fs-3 fw-bold">${game.name}</h3>
                     <p class="card-text">${game.description}</p>
                     <a href="${game.url}"><button class="btn btn-primary">Otevřít</button></a>
                 </div>
-            </article>
-        </a>`
+            </div>
+        </div>`
     }
 }
 
